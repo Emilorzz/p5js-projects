93c87b65f3d8f9a6a56a7a492f40f24164f88e01
Emilorzz <emil69h@gmail.com>
2024-06-04 20:25:46 +0200

    now shows all intermediate iterations until a valid grid is found, instead of freezing

--- sketch.js
+++ sketch.js
@@ -77,7 +77,8 @@ function draw() {
   if (pathFound) {
     showSteps();
   } else {
-    noPath();
+    initGrid();
+    algorithms[selectedAlg](start);
   }
 
   showHelp();
@@ -408,28 +409,28 @@ function drawPath(arr, aux, color) {
 }
 
 function initGrid() {
-  do {
-    index = 0;
-    path = [];
-    full = [];
-    pathaux = [];
-    fullaux = [];
-    setColors();
+  // do {
+  index = 0;
+  path = [];
+  full = [];
+  pathaux = [];
+  fullaux = [];
+  setColors();
 
-    pathFound = false;
-    grid = [];
-    for (let i = 0; i < cols; i++) {
-      grid[i] = [];
-      for (let j = 0; j < rows; j++) {
-        grid[i][j] = new Cell(i, j);
-      }
+  pathFound = false;
+  grid = [];
+  for (let i = 0; i < cols; i++) {
+    grid[i] = [];
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j);
     }
+  }
 
-    positions[selectedPos].function();
-    initWalls();
-    initNeighbors();
-    // algorithms[selected](start);
-  } while (!algorithms[selectedAlg](start) || path.length < 20);
+  positions[selectedPos].function();
+  initWalls();
+  initNeighbors();
+  // algorithms[selected](start);
+  // } while (!algorithms[selectedAlg](start) || path.length < 20);
 }
 
 function posRandom() {
@@ -790,6 +791,10 @@ function keyPressed() {
       break;
     case 55:
       coverage = 0.7;
+      initGrid();
+      break;
+    case 56:
+      coverage = 0.8;
       initGrid();
       break;
   }
